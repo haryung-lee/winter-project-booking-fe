6805c34d2d6df3a52a3794742af7443e6b397cfd
leeharyung <lhr4884@naver.com>
2023-03-10 21:03:31 +0900

    fix: delete unused code

--- hooks/useDebounce.ts
+++ hooks/useDebounce.ts
@@ -1,15 +1,3 @@
-// export function useDebounce<F extends (...args: any[]) => any>(
-//   func: F,
-//   delay: number
-// ) {
-//   let timeout: NodeJS.Timeout
-//   return function (this: ThisParameterType<F>, ...args: Parameters<F>) {
-//     const context = this
-//     clearTimeout(timeout)
-//     timeout = setTimeout(() => func.apply(context, args), delay)
-//   }
-// }
-
 import { useEffect, useState } from 'react'
 
 export function useDebounce<T>(value: T, delay: number): T {
