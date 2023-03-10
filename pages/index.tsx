fae555a0634e18b39bf43091d10195e6a49aee07
leeharyung <lhr4884@naver.com>
2023-03-10 21:03:53 +0900

    fix: remove head tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,10 @@
-import Head from 'next/head'
-
 import BaseLayout from 'components/common/base-layout'
 import HomeContainer from 'components/home'
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>인천대 책 정보 제공 서비스</title>
-        <meta name="description" content="인천대 책 정보 제공 서비스" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <BaseLayout>
-        <HomeContainer />
-      </BaseLayout>
-    </>
+    <BaseLayout>
+      <HomeContainer />
+    </BaseLayout>
   )
 }
